fdced8e2a8b3179ee7135f44eec32bc8ea88c380
IzzyMeow <jibakumaki69@gmail.com>
2025-04-30 19:25:22 +0700

    feat: add login function and login.csv implementation
    recursive if password and username not  found

--- src/main.c
+++ src/main.c
@@ -1,10 +1,22 @@
 // Include any necessary headers
 #include <stdio.h>
+#include <string.h>
 #include "header/tes.h"
+#include "header/login.h"
 
-int main() {
+
+int main()
+{
     // Your code goes here
-    printf("Hello, World!\n");
-    tes();
+    char promt[1001];
+    printf("========================================\n");
+    printf(">>> ");
+    scanf("%s", &promt);
+    if (strcmp(promt,"LOGIN") == 0)
+    {
+        login();
+    }
+    //printf("Hello, World!\n");
+    //tes();
     return 0;
 }
