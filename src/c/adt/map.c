db2b781bfefb690aa5d96ebc03e3c4eaae238cb7
YeyThePotatoMan <klocesaragih@gmail.com>
2025-06-01 02:24:27 +0700

    fix: map fix

--- src/c/adt/map.c
+++ src/c/adt/map.c
@@ -1,100 +1,3 @@
-// #include "../../header/adt/map.h"
-// #include <stdlib.h>
-// #include "../header/random.h"
-
-// void map_create(Map t, int key,int room){
-//     t->key = key;
-//     t->prior = random_lcg(key,key);
-//     t->value = room;
-//     t->l = NULL;
-//     t->r = NULL;
-// }
-
-// Map map_findMap(Map t,int key){
-//     /* Jika t == NULL, maka key tidak ditemukan */
-//     if( t == NULL ) {
-//         return NULL;
-//     }
-//     /* Jika key sama, berarti prior ditemukan */
-//     if( t->key == key ) return t;
-
-//     if( key < t->key ) return map_findMap(t->l,key);
-//     else return map_findMap(t->r,key);
-// }
-
-// void map_split(Map t, int key, pMap l, pMap r) {
-//     /* Jika sampai di leaf, berhenti*/
-//     if (t == NULL){
-//         *l = NULL;
-//         *r = NULL;
-//     }
-//     /* Jika key sekarang <= key_param split child kanan */
-//     else if (t->key <= key){
-//         map_split(t->r, key, &t->r, r);
-//         *l = t;
-//     }
-//     /* Jika key sekarang > key_param split child kiri */
-//     else{
-//         map_split(t->l, key, l, &t->l);
-//         *r = t;
-//     }
-// }
-
-// void map_insertNewMap(pMap t, Map it){
-//     /* Jika berada di akhir tree, buat node baru*/
-//     if ( (*t) == NULL) *t = it;
-    
-//     /*  
-//         Jika priority node sekarang > priority node baru:
-//         Split tree menjadi 2 bagian:
-//         1. tree dengan node key < key node baru
-//         2. tree dengan node key >= key node baru
-//         Kemudian, gabungkan kedua tree menjadi subtree dari node baru
-//     */
-//     else if (it->prior > (*t)->prior){
-//         map_split(*t, it->key, &it->l, &it->r);
-//         *t = it;
-//     }
-    
-//     /*
-//         Jika key node sekarang <= key node baru, traverse ke node kanan
-//         Jika tidak, traverse ke node kiri
-//     */
-//     else{
-//         if( (*t)->key <= it->key ) map_insertNewMap(&(*t)->r, it);
-//         else map_insertNewMap(&(*t)->l, it);
-//     }
-// }
-
-// void map_insertData(pMap t, int idDokter, int idPasien, int room) {
-//     /* Jika tidak terdapat map dengan id yang sesuai, buat node map baru*/
-//     if (*t == NULL) {
-//         *t = malloc(sizeof(NodeMap));
-//         map_create(*t, idDokter, room);
-//         queue_push(&((*t)->data), idPasien);
-//         return;
-//     }LOGIN
-
-//     /* Cari posisi map dengan id yang sesuai*/
-//     Map mp = map_findMap(*t, idDokter);
-
-//     /* Jika tidak terdapat map dengan id sesuai, buat node baru*/
-//     if (mp == NULL) {
-//         Map newMap = malloc(sizeof(NodeMap));
-//         map_create(newMap, idDokter, room);
-//         map_insertNewMap(t, newMap);
-//         mp = newMap;
-//     }
-    
-//     /* Push data baru ke queue berdasarkan posisi map yang sesuai*/
-//     queue_push(&(mp->data), idPasien);
-// }
-
-// void map_popData(Map t,int idDokter){
-//     Map mp = map_findMap(t,idDokter);
-//     queue_pop(&(mp->data));
-// }
-
 #include "../../header/adt/map.h"
 #include <stdlib.h>
 
@@ -157,4 +60,44 @@ void map_insert(pMap t, char* key, int value){
     Map newMap = malloc(sizeof(NodeMap));
     map_create(newMap, key, value);
     map_insertNewMap(t, newMap);
+}
+
+void map_delete(pMap t, const char* key) {
+    if (*t == NULL) return;
+
+    int cmp = strcmp(key, (*t)->key);
+    if (cmp == 0) {
+        Map temp = *t;
+        map_merge(t, (*t)->l, (*t)->r);
+        free(temp);
+    } else if (cmp < 0) {
+        map_delete(&(*t)->l, key);
+    } else {
+        map_delete(&(*t)->r, key);
+    }
+}
+
+void map_merge(pMap t, Map l, Map r) {
+    if (l == NULL && r == NULL) {
+        *t = NULL;
+        return;
+    }
+
+    if (l == NULL) {
+        *t = r;
+        return;
+    }
+
+    if (r == NULL) {
+        *t = l;
+        return;
+    }
+
+    if (l->prior > r->prior) {
+        map_merge(&l->r, l->r, r);
+        *t = l;
+    } else {
+        map_merge(&r->l, l, r->l);
+        *t = r;
+    }
 }
